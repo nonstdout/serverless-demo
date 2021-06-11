d0d38828b96ff604a551e8669d2356fb92c54a50
nonstdout <csalisbury@natilik.com>
2021-06-11 14:25:55 +0100

    change

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ addEventListener("fetch", (event) => {
             status: 200,
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
-                "Access-Control-Allow-Origin": "*"
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Origin, Content-Type, Accept"
             },
         },
     );
-    console.log(response)
     event.respondWith(response);
 });
