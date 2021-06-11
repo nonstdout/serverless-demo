d5b86232864bec3cc3b868309b0c4d98b7ea7fd9
nonstdout <csalisbury@natilik.com>
2021-06-12 00:40:09 +0100

    updated

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 addEventListener("fetch", (event) => {
     const response = new Response(
-        JSON.stringify({ message: "couldn't process your request" }),
+        JSON.stringify({ message: "Hi!" }),
         {
             status: 200,
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-                "Access-Control-Allow-Headers": "Origin, Content-Type, Accept"
+                "Access-Control-Allow-Methods": "*",
+                "Access-Control-Allow-Headers": "*"
             },
         },
     );
