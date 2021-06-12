42825e4c988c31b656bd8bf93bcab55344de76c5
nonstdout <csalisbury@natilik.com>
2021-06-12 17:53:49 +0100

    without cors

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ addEventListener("fetch", (event) => {
 async function handleRequest(request) {
 
     const contentType = request.headers.get("content-type");
-    console.log(request.headers)
+    console.log(await request)
     const responseInit = {
         headers: {
-            "Content-Type": "application/json; charset=utf-8",
+            "Content-Type": "text/plain; charset=utf-8",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "*",
             "Access-Control-Allow-Headers": "*",
@@ -20,9 +20,9 @@ async function handleRequest(request) {
     // if (contentType.includes("application/json")) {
     //     console.log(json)
     // }
-    const json = await request.json();
-    console.log(json)
-    return new Response(JSON.stringify({ json }, null, 2), responseInit);
+    // const json = await request.json();
+    // console.log(json)
+    return new Response({ status: 200, headers: { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "http://localhost:5000", } });
     // const response = new Response(
     //     JSON.stringify({ message: "Hi!" }),
     //     {
